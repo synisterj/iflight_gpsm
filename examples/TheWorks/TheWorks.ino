05392d0e2ef8c4986883815a0e81e1d15b1b1efc
Jason Edelbrock <synisterj@users.noreply.github.com>
2021-09-29 17:44:33 -0400

    Update TheWorks.ino

--- examples/TheWorks/TheWorks.ino
+++ examples/TheWorks/TheWorks.ino
@@ -8,8 +8,7 @@
  * Defining these for ease...
  */
 #define DEBUG true
-#define APP_VERSION "1.2.4"
-#define LIB_VERSION iFlightGPSLib::libVersion()
+#define VERSION "1.1.0"
 /* Constant variables for rx/tx 
  * pins, LCD pins, Arduino and
  * GPS module baud rates.
@@ -17,22 +16,22 @@
 const int RX = 8, TX = 9;
 const int RS = 12, EN = 11, D4 = 5, D5 = 4, D6 = 3, D7 = 2, btn = 7;
 const uint32_t GB = 9600, AB = 115200;
-/*
- * For stats that update every 5 seconds. 
- */
-unsigned long last = 0UL;
 /* 
  * For the push button
- * scroll menu's position.
+ * scroll menu.
  */
-int btn_pos = 1;
-int btn_state = 0;
+int btn_ctr = 1;
+int buttonState = 0;
 /* 
  * Declaring variables of class types.
  */
 iFlightGPSLib GPS;
 SoftwareSerial GPSMS(RX, TX);
 LiquidCrystal LCD( RS, EN, D4, D5, D6, D7 );
+/*
+ * For stats that update every 5 seconds. 
+ */
+unsigned long last = 0UL;
 /* 
  * Initiation... 
  */
@@ -45,209 +44,190 @@ void setup()
   }
   LCD.begin(16, 2);
   GPSMS.begin( GB );
-  /*
-   * Display my custom iFlightGPSLib version.
-   */
-   LCD.clear();
-   _write("Using Application", 0, 0, false, false);
-   _write("Version: " + String( APP_VERSION ), 0, 1, false, false);
-   delay( 1000 );
-   LCD.clear();
-   _write("Using iFlightGPSLib", 0, 0, false, false);
-   _write("Version: " + String( LIB_VERSION ), 0, 1, false, false);
-   delay( 1000 );
-   LCD.clear();
-   if ( DEBUG ) { Serial.print( F( "Using application version: " ) ); Serial.println( APP_VERSION ); }
-   if ( DEBUG ) { Serial.print( F( "Using iFlightGPSLib version: " ) ); Serial.println( LIB_VERSION ); }
+  if ( DEBUG ) {
+    Serial.print( F( "Using iFlightGPSLib version: " ) ); Serial.println( iFlightGPSLib::libraryVersion() );
+    Serial.println();
+  }
 }
 /*
  * Main loop...
  */
 void loop()
 {
-  btn_state = digitalRead(btn);
-  if ( btn_state == HIGH ) {
-    btn_pos++;  
+  buttonState = digitalRead(btn);
+  if ( buttonState == HIGH ) {
+    btn_ctr++;  
   }
   /*
    * Switch statement for button scroll menu.
    */
-  switch ( btn_pos ) {
-  case 1: // If position = 1
-    if ( DEBUG ) { Serial.println( "DEBUG: #1 Location" ); } // If DEBUG is true print this to the serial monitor.
-    LCD.clear(); // Clear the LCD.
-    _write( "#1 Location", 0, 0, false, false ); // Write to the LCD.
-    while ( btn_pos == 1 ) { // While the button position remains at 1...
-      while ( GPSMS.available() > 0 ) // Check if GPSM serial is available.
-        GPS.encode( GPSMS.read() ); // If and whille available, read the data.
-        if ( GPS.location.isUpdated() ) // If data has been updated...
+  switch (btn_ctr) {
+  case 1:
+    Serial.println("Option 1 selected...");
+    LCD.clear();
+    while ( btn_ctr == 1 ) {
+      while (GPSMS.available() > 0)
+        GPS.encode(GPSMS.read());
+        if (GPS.location.isUpdated())
         {
-          _write( lcd_display_loc(), 0, 1, false, true ); // Write it to the LCD.
+          lcd_display_loc();
         }
-      btn_state = digitalRead( btn ); // Check our button state(HIGH 5v 10kohm) to see if it's been pressed.
-      delay( 100 ); // Give a little delay to read the button if pressed.
-      if ( btn_state == HIGH ) { // If the button has been pressed.
-        btn_pos++; // Increment our button position.
+      buttonState = digitalRead(btn);
+      delay(100);
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0; // Return our button state to 0.
-    break; // Break out of the case or button position.
+    buttonState = 0;
+    break;
   case 2:
-    if ( DEBUG ) { Serial.println("DEBUG: #2 Date"); }
+    Serial.println("Option 2 selected...");
     LCD.clear();
-    _write("#2 Date", 0, 0, false, false);
-    while ( btn_pos == 2 ) {
+    while ( btn_ctr == 2 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.date.isUpdated())
           {
             lcd_display_date();
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
   case 3:
-    if ( DEBUG ) { Serial.println("DEBUG: #3 Time"); }
+    Serial.println("Option 3 selected...");
     LCD.clear();
-    _write("#3 Time", 0, 0, false, false);
-    while ( btn_pos == 3 ) {
+    while ( btn_ctr == 3 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.time.isUpdated())
           {
             lcd_display_time(); 
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
   case 4:
-    if ( DEBUG ) { Serial.println("DEBUG: #4 Speed"); }
+    Serial.println("Option 4 selected...");
     LCD.clear();
-    _write("#4 Speed", 0, 0, false, false);
-    while ( btn_pos == 4 ) {
+    while ( btn_ctr == 4 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.speed.isUpdated())
           {
             lcd_display_spd();  
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
   case 5:
-    if ( DEBUG ) { Serial.println("DEBUG: #5 Course"); }
+    Serial.println("Option 5 selected...");
     LCD.clear();
-    _write("#5 Course", 0, 0, false, false);
-    while ( btn_pos == 5 ) {
+    while ( btn_ctr == 5 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.course.isUpdated())
           {
             lcd_display_course();  
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
   case 6:
-    if ( DEBUG ) { Serial.println("DEBUG: #6 Altitude"); }
+    Serial.println("Option 6 selected...");
     LCD.clear();
-    _write("#6 Altitude", 0, 0, false, false);
-    while ( btn_pos == 6 ) {
+    while ( btn_ctr == 6 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.altitude.isUpdated())
           {
             lcd_display_altitude();  
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
   case 7:
-    if ( DEBUG ) { Serial.println("DEBUG: #7 Satellites"); }
+    Serial.println("Option 7 selected...");
     LCD.clear();
-    _write("#7 Satellites", 0, 0, false, false);
-    while ( btn_pos == 7 ) {
+    while ( btn_ctr == 7 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.satellites.isUpdated())
           {
             lcd_display_satellites();  
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
     case 8:
-    if ( DEBUG ) { Serial.println("DEBUG: #8 HDOP"); }
+    Serial.println("Option 8 selected...");
     LCD.clear();
-    _write("#8 HDOP", 0, 0, false, false);
-    while ( btn_pos == 8 ) {
+    while ( btn_ctr == 8 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (GPS.hdop.isUpdated())
           {
             lcd_display_hdop();  
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
     case 9:
-    if ( DEBUG ) { Serial.println("DEBUG: #9 Diagnostics"); }
+    Serial.println("Option 9 selected...");
     LCD.clear();
-    _write("#9 Diagnostics", 0, 0, false, false);
-    while ( btn_pos == 9 ) {
+    while ( btn_ctr == 9 ) {
       while (GPSMS.available() > 0)
         GPS.encode(GPSMS.read());
           if (millis() - last > 5000)
           {
             _diag();  
           }
-      btn_state = digitalRead(btn);
+      buttonState = digitalRead(btn);
       delay(100);
-      if ( btn_state == HIGH ) {
-        btn_pos++;  
+      if ( buttonState == HIGH ) {
+        btn_ctr++;  
       }
     }
-    btn_state = 0;
+    buttonState = 0;
     break;
   default:
     Serial.println("Default option selected, returning to option 1...");
-    btn_pos = 1;
-    btn_state = 0;
+    btn_ctr = 1;
+    buttonState = 0;
     break;
   }
 }
